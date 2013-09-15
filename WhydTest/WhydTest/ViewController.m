3ca7af0c4da508517807233b2e2e4c8bceee8d15
Adrien Guffens <adrien.guffens@epitech.eu>
2013-09-15 17:06:45 +0200

    YouTube integration with embed WebView

--- WhydTest/WhydTest/ViewController.m
+++ WhydTest/WhydTest/ViewController.m
@@ -42,9 +42,9 @@ static NSString *soundcloundClientId = @"4a35610ca12c56aa757a2b3c140215a6";
 	track.type = soundclound;
 	
 	Track *track2 = [[Track alloc] init];
-	track2.name = @"Forever Dolphin Love";
+	track2.name = @"Agoria 50 min Boiler Room Mix at ADE 2012";
 	track2.duration = 100.0;
-	track2.url = @"http://www.youtube.com/embed/OSAsuy_UwO8";
+	track2.url = @"http://www.youtube.com/embed/l5Qem9SAQZY";
 	track2.type = youtube;
 	
 	Track *track3 = [[Track alloc] init];
@@ -53,10 +53,19 @@ static NSString *soundcloundClientId = @"4a35610ca12c56aa757a2b3c140215a6";
 	track3.url = @"";
 	track3.type = soundclound;
 	
+	////www.youtube.com/embed/O-B46mrOtCM
+	
+	Track *track4 = [[Track alloc] init];
+	track4.name = @"Julian Jeweil - Don't Think (Original Mix)";
+	track4.duration = 100.0;
+	track4.url = @"http://www.youtube.com/embed/O-B46mrOtCM";
+	track4.type = youtube;
+	
 	
 	[self.tracksList addObject:track];
 	[self.tracksList addObject:track2];
 	[self.tracksList addObject:track3];
+	[self.tracksList addObject:track4];
 	
 	//DEBUG:
 	NSLog(@"%s - tracksList: %@", __PRETTY_FUNCTION__, self.tracksList);
@@ -76,14 +85,39 @@ static NSString *soundcloundClientId = @"4a35610ca12c56aa757a2b3c140215a6";
 - (void)updateViewForTheCurrentTrack {
 	//INFO: getting the track
 	self.currentTrack = [self.tracksList objectAtIndex:self.position];
+	NSLog(@"%s | self.currentTrack: %@ at position: %d", __PRETTY_FUNCTION__, self.currentTrack, self.position);
     
 	//INFO: setting up informations ---> do it on the main thread
 	{
 		self.nameLabel.text = self.currentTrack.name;
 		
-		NSString *buttonTitle = (self.isPlaying == NO ? @"Pause" : @"Play");
+		NSString *buttonTitle = (self.isPlaying == YES ? @"Pause" : @"Play");
 		[self.playPauseButton setTitle:buttonTitle forState:UIControlStateNormal];
 		
+		UIColor *backgroundColor = nil;
+		switch (self.currentTrack.type) {
+			case soundclound:
+			{
+				backgroundColor = [UIColor colorWithRed:254.0 / 255.0 green:70.0 / 255.0 blue:0.0 / 255.0 alpha:1.0];
+			}
+				break;
+			case youtube:
+			{
+				backgroundColor = [UIColor colorWithRed:197.0 / 255.0 green:26.0 / 255.0 blue:32.0 / 255.0 alpha:1.0];
+			}
+				break;
+			case other:
+			{
+				backgroundColor = [UIColor whiteColor];
+			}
+				break;
+				
+			default:
+				break;
+		}
+		dispatch_async(dispatch_get_main_queue(), ^{
+			[self.view setBackgroundColor:backgroundColor];
+		});
 	}
 }
 
@@ -99,15 +133,17 @@ static NSString *soundcloundClientId = @"4a35610ca12c56aa757a2b3c140215a6";
             
         case youtube:
         {
-            UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, self.playerView.frame.size.width, self.playerView.frame.size.height)];
+			
+            UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, self.playerView.bounds.size.width, self.playerView.bounds.size.height)];
             webView.delegate = self;
+			[webView setBackgroundColor:[UIColor clearColor]];
             //TODO: do a custom UIWebView class
             
             [self.playerView addSubview:webView];
             
             
             [self embedYouTubeInWebView:self.currentTrack.url theWebView:webView];
-
+			
             
             
             
@@ -140,20 +176,37 @@ static NSString *soundcloundClientId = @"4a35610ca12c56aa757a2b3c140215a6";
 
 - (void)embedYouTubeInWebView:(NSString*)url theWebView:(UIWebView *)aWebView {
     NSLog(@"%s %@", __PRETTY_FUNCTION__, [aWebView description]);
-    NSString *embedHTML = @"\
-    <html><head>\
-    <style type=\"text/css\">\
-    body {\
-    background-color: transparent;\
-    color: white;\
-    }\
-    </style>\
-    </head><body style=\"margin:0\">\
-    <embed id=\"yt\" src=\"%@\" type=\"application/x-shockwave-flash\" \
-    width=\"%0.0f\" height=\"%0.0f\"></embed>\
-    </body></html>";
+	/*
+	 NSString *embedHTML = @"\
+	 <html><head>\
+	 <style type=\"text/css\">\
+	 body {\
+	 background-color: transparent;\
+	 color: white;\
+	 }\
+	 </style>\
+	 </head><body style=\"margin:0\">\
+	 <embed id=\"yt\" src=\"%@\" type=\"application/x-shockwave-flash\" \
+	 width=\"%0.0f\" height=\"%0.0f\"></embed>\
+	 </body></html>";
+	 */
+	
+	NSString *embedHTML  = [NSString stringWithFormat:@"\
+							<html>\
+							<head>\
+							<style type=\"text/css\">\
+							iframe {}\
+							body {background-color:#000; margin:0;}\
+							</style>\
+							</head>\
+							<body>\
+							<iframe width=\"%0.0f\" height=\"%0.0f\" src=\"%@\" frameborder=\"0\" allowfullscreen></iframe>\
+							</body>\
+							</html>", aWebView.frame.size.width, aWebView.frame.size.height, url];
     
     NSString* html = [NSString stringWithFormat:embedHTML, url, aWebView.frame.size.width, aWebView.frame.size.height];
+	
+	NSLog(@"%s | html: %@", __PRETTY_FUNCTION__, html);
     
     [aWebView loadHTMLString:html baseURL:nil];
 }
@@ -163,6 +216,8 @@ static NSString *soundcloundClientId = @"4a35610ca12c56aa757a2b3c140215a6";
 - (IBAction)prevHandler:(id)sender {
 	//INFO: setting prev sound to play
 	self.position = ((self.position - 1) < 0) ? ([self.tracksList count] - 1) : self.position - 1;
+	NSLog(@"%s | self.position: %d", __PRETTY_FUNCTION__, self.position);
+	
 	[self updateViewForTheCurrentTrack];
 }
 
@@ -171,6 +226,7 @@ static NSString *soundcloundClientId = @"4a35610ca12c56aa757a2b3c140215a6";
 		self.isPlaying = YES;
 		
 		[self updateViewForTheCurrentTrack];
+		[self playCurrentTrack];
         
 	}
 	else {
@@ -183,6 +239,7 @@ static NSString *soundcloundClientId = @"4a35610ca12c56aa757a2b3c140215a6";
 - (IBAction)nextHandler:(id)sender {
 	//INFO: setting next sound to play
 	self.position = (self.position + 1) >= [self.tracksList count] ? 0 : self.position + 1;
+	NSLog(@"%s | self.position: %d", __PRETTY_FUNCTION__, self.position);
     
 	[self updateViewForTheCurrentTrack];
 }
